12010c3c75b4acd9a18e6508a2d894ae97c9b9fb
vuk.trifunovic <vuk.tsa.mes@gmail.com>
2025-10-14 21:01:02 +0200

    center add folder/list buttons on overview tab

--- src/components/Content/Overview/Overview.vue
+++ src/components/Content/Overview/Overview.vue
@@ -14,36 +14,42 @@ const openCreateForm = (createFormName) => {
 </script>
 
 <template>
-    <div class="grid grid-cols-2 gap-10">
-        <div v-if="spaceStore.getFolders.length">
-            <h1 class="!ml-2 !mb-1 text-xl !font-medium">Folders</h1>
-            <div class="rounded-lg min-h-[300px] p-2 grid grid-cols-2 auto-rows-min gap-2">
-                <div v-for="folder in spaceStore.getFolders" @click="() => spaceStore.setCurrentFolder(folder, true)" class="rounded-sm border-2 border-gray-600 text-gray-400 hover:text-gray-200 hover:border-gray-200 cursor-pointer flex items-center pl-2 place-self-start w-full py-2">
-                    <span class="flex gap-2 items-center !font-medium"><FolderIcon class="w-5 h-5" /> {{ folder.name }}</span>
+    <div class="grid grid-cols-2 gap-10 min-h-[200px]">
+        <div>
+            <div v-if="spaceStore.getFolders.length">
+                <h1 class="!ml-2 !mb-1 text-xl !font-medium">Folders</h1>
+                <div class="rounded-lg min-h-[300px] p-2 grid grid-cols-2 auto-rows-min gap-2">
+                    <div v-for="folder in spaceStore.getFolders" @click="() => spaceStore.setCurrentFolder(folder, true)" class="rounded-sm border-2 border-gray-600 text-gray-400 hover:text-gray-200 hover:border-gray-200 cursor-pointer flex items-center pl-2 place-self-start w-full py-2">
+                        <span class="flex gap-2 items-center !font-medium"><FolderIcon class="w-5 h-5" /> {{ folder.name }}</span>
+                    </div>
+                </div>
+            </div>
+            <div v-else class="w-full h-full flex items-center justify-center">
+                <div
+                    class="w-[200px] text-center text-xl !font-medium rounded-sm border-3 border-gray-600 text-gray-400 hover:text-gray-200 hover:border-gray-200 cursor-pointer pl-2  py-2"
+                    @click="() => openCreateForm('CreateFolder')"
+                >
+                    Add Folder +
                 </div>
             </div>
         </div>
-        <div
-            v-else
-            class="w-[200px] text-center text-xl !font-medium rounded-sm border-3 border-gray-600 text-gray-400 hover:text-gray-200 hover:border-gray-200 cursor-pointer pl-2  py-2"
-            @click="() => openCreateForm('CreateFolder')"
-        >
-            Add Folder +
-        </div>
-        <div v-if="spaceStore.getFolderlessLists.length">
-            <h1 class="!ml-2 !mb-1 text-xl !font-medium">Lists</h1>
-            <div class="rounded-lg min-h-[300px] p-2 grid grid-cols-2 auto-rows-min gap-2">
-                <div v-for="list in spaceStore.getFolderlessLists" @click="() => spaceStore.setCurrentList(list)" class="rounded-sm border-2 border-gray-600 text-gray-400 hover:text-gray-200 hover:border-gray-200 cursor-pointer flex items-center pl-2 place-self-start w-full py-2">
-                    <span class="flex gap-2 items-center !font-medium"><ListBulletIcon class="w-5 h-5" /> {{ list.name }}</span>
+        <div class="border-l-2 border-gray-500">
+            <div v-if="spaceStore.getFolderlessLists.length">
+                <h1 class="!ml-2 !mb-1 text-xl !font-medium">Lists</h1>
+                <div class="rounded-lg min-h-[300px] p-2 grid grid-cols-2 auto-rows-min gap-2">
+                    <div v-for="list in spaceStore.getFolderlessLists" @click="() => spaceStore.setCurrentList(list)" class="rounded-sm border-2 border-gray-600 text-gray-400 hover:text-gray-200 hover:border-gray-200 cursor-pointer flex items-center pl-2 place-self-start w-full py-2">
+                        <span class="flex gap-2 items-center !font-medium"><ListBulletIcon class="w-5 h-5" /> {{ list.name }}</span>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div
-            v-else
-            class="w-[200px] h-fit text-center text-xl !font-medium rounded-sm border-3 border-gray-600 text-gray-400 hover:text-gray-200 hover:border-gray-200 cursor-pointer pl-2  py-2"
-            @click="() => openCreateForm('CreateList')"
-            >
-            Add List +
+            <div v-else class="w-full h-full flex items-center justify-center">
+                <div
+                    class="w-[200px] h-fit text-center text-xl !font-medium rounded-sm border-3 border-gray-600 text-gray-400 hover:text-gray-200 hover:border-gray-200 cursor-pointer pl-2  py-2"
+                    @click="() => openCreateForm('CreateList')"
+                >
+                    Add List +
+                </div>
+            </div>
         </div>
     </div>
 </template>
